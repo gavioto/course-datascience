aa71a64ea98c6075e75cc667ec18396250622bd9
gavioto <acorralortiz@gmail.com>
2014-10-23 19:39:25 +0200

    initial commit

---
+++ provision_python.sh
@@ -0,0 +1,13 @@
+sudo apt-get update
+sudo apt-get -y install python-pip python-dev build-essential 
+sudo apt-get -y install libzmq-dev
+sudo pip install --upgrade pip 
+sudo pip install --upgrade virtualenv
+sudo apt-get -y install numpy
+sudo apt-get -y install python-matplotlib python-scipy python-pandas python-sympy python-nose
+sudo pip install pyzmq jinja2 tornado
+sudo pip install ipython
+sudo pip install --upgrade ipython[all]
+#cd /vagrant && ipython notebook --pylab=inline --ip=0.0.0.0 --no-browser
+
+ 
